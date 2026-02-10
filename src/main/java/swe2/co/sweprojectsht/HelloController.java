95909181a7715cdff8e4a78c6e75775ca32e2fc6
tirea <tirearuwajoye1@gmail.com>
2026-02-10 13:46:22 +0000

    New Project with JavaFx insatlled using maven as build Config with Java21

---
+++ src/main/java/swe2/co/sweprojectsht/HelloController.java
@@ -0,0 +1,14 @@
+package swe2.co.sweprojectsht;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
