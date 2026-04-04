7c689abc77d894fd6b7d8a662b094a68e54dd1a5
tirea <tirearuwajoye1@gmail.com>
2026-04-04 01:54:45 +0100

    removed some redundant cases

--- src/test/java/swe2/co/sweprojectsht/FxTestUtils.java
+++ src/test/java/swe2/co/sweprojectsht/FxTestUtils.java
@@ -1,19 +1,14 @@
 package swe2.co.sweprojectsht;
 
 import javafx.application.Platform;
-import javafx.scene.Node;
-import javafx.scene.Scene;
-import javafx.scene.control.ButtonBase;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.DialogPane;
-import javafx.stage.Window;
-
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
 class FxTestUtils {
+
+
 
     static void initJavaFx() {
         try {
@@ -72,59 +67,6 @@ class FxTestUtils {
         }
 
         return resultHolder.value;
-    }
-
-    static DialogPane findShowingDialogPaneByHeader(String headerText) {
-        for (Window window : Window.getWindows()) {
-            if (!window.isShowing()) {
-                continue;
-            }
-
-            Scene scene = window.getScene();
-            if (scene == null) {
-                continue;
-            }
-
-            if (scene.getRoot() instanceof DialogPane dialogPane) {
-                if (headerText.equals(dialogPane.getHeaderText())) {
-                    return dialogPane;
-                }
-            }
-        }
-        return null;
-    }
-
-    static DialogPane findShowingDialogPaneByTitle(String title) {
-        for (Window window : Window.getWindows()) {
-            if (!window.isShowing()) {
-                continue;
-            }
-
-            Scene scene = window.getScene();
-            if (scene == null) {
-                continue;
-            }
-
-            if (scene.getRoot() instanceof DialogPane dialogPane) {
-                if (dialogPane.getScene() != null &&
-                        dialogPane.getScene().getWindow() != null &&
-                        title.equals(dialogPane.getScene().getWindow().getScene().getWindow().getClass().getSimpleName())) {
-                    return dialogPane;
-                }
-            }
-        }
-        return null;
-    }
-
-    static void clickDialogButton(DialogPane dialogPane, String buttonText) {
-        for (ButtonType buttonType : dialogPane.getButtonTypes()) {
-            if (buttonText.equals(buttonType.getText())) {
-                Node button = dialogPane.lookupButton(buttonType);
-                ((ButtonBase) button).fire();
-                return;
-            }
-        }
-        fail("Could not find dialog button with text: " + buttonText);
     }
 
     @FunctionalInterface
